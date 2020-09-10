8873e5aba37c1e07f9e7be603f5c77cb558ed14c
nvitas <vsnikonov@kassa24.kz>
2020-09-10 22:23:59 +0600

    Релиз для Валихана

--- src/views/examples/Managers.js
+++ src/views/examples/Managers.js
@@ -1,42 +1,184 @@
 import React from 'react';
 
 // reactstrap components
-import { Card, Container, Row, CardHeader } from "reactstrap";
+import { Card,
+  Container,
+  Row,
+  CardHeader,
+  Table,
+  Button,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input
+} from "reactstrap";
+import moment from 'moment';
 // core components
 import Header from "components/Headers/Header.js";
-import Manager from "components/ManagersPage/Manager.js"
 const managers = [
-  { name: 'm1_name', surname: 'm1_surname', username : 'manager_1', date: '26.08.20', status: 'status', groups : [true, false, false] },
-  { name: 'm2_name', surname: 'm2_surname', username : 'manager_2', date: '26.08.20', status: 'status', groups : [false, true, false] },
-  { name: 'm3_name', surname: 'm3_surname', username : 'manager_3', date: '26.08.20', status: 'status', groups : [false, false, true] },
-  { name: 'm4_name', surname: 'm4_surname', username : 'manager_4', date: '26.08.20', status: 'status', groups : [true, true, false] },
-  { name: 'm5_name', surname: 'm5_surname', username : 'manager_5', date: '26.08.20', status: 'status', groups : [true, false, true] },
-  { name: 'm6_name', surname: 'm6_surname', username : 'manager_6', date: '26.08.20', status: 'status', groups : [false, true, true] },
-  { name: 'm7_name', surname: 'm7_surname', username : 'manager_7', date: '26.08.20', status: 'status', groups : [true, true, true] },
-  { name: 'm8_name', surname: 'm8_surname', username : 'manager_8', date: '26.08.20', status: 'status', groups : [false, false, false]},
+  {id:1, userID:2,	chatID:435274667, firstname:'Виталий', lastname:'Никонов', username:'N_Vitas', reghash:'1röÖr7', date:'2020-08-19 06:08:30',	status:2},
+  {id:2, userID:3,	chatID:553603486, firstname:'Eugene', lastname:'Antonov', username:'', reghash:'6Å9qev', date:'2020-09-10 10:26:50',	status:2},
+  {id:4, userID:4,	chatID:1233452136, firstname:'Olga', lastname:'Antonova', username:'', reghash:'SMESxÖ', date:'2020-09-10 10:31:19',	status:2},
+  {id:5, userID:0,	chatID:1219646264, firstname:'Metafora', lastname:'', username:'', reghash:'ELH2cl', date:'2020-09-10 10:35:46',	status:1},
+  {id:6, userID:0,	chatID:1255719428, firstname:'Анастасия',	lastname:'Тесленко', username:'', reghash:'7LpGS0', date:'2020-09-10 10:44:43',	status:1},
+  {id:7, userID:0,	chatID:532031398, firstname:'Metafora', lastname:'', username:'', reghash:'B7145C', date:'2020-09-10 10:45:13',	status:1},
+  {id:8, userID:0,	chatID:374090104, firstname:'Nursulu', lastname:'K', username:'', reghash:'6YWUÅv', date:'2020-09-10 10:59:34',	status:1},
+  {id:9, userID:0,	chatID:800646519, firstname:'Полина', lastname:'', username:'', reghash:'pOBdÖa', date:'2020-09-10 11:03:16',	status:1},
 ]
+const statuses = ['Заблокирован', 'Не подтвержден', 'Активный']
 class Managers extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
         managers : managers,
+        modal: {
+          open: false,
+          title: 'Удаление менеджера',
+          body: 'Вы действительно хотите удалить менеджера?',
+          ok: this.toggle.bind(this),
+          okTitle: 'Да',
+          cancel: this.toggle.bind(this),
+          cancelTitle: 'Отмена',
+        },
+        firstname: '',
+        lastname:'',
+        username:'',
+        status:0,
     }
-    
-    this.deleteManager = this.deleteManager.bind(this)
+    this.deleteManager = this.deleteManager.bind(this);
+    this.editManager = this.editManager.bind(this);
+    this.changeLogin = this.changeLogin.bind(this);
+    this.changeName = this.changeName.bind(this);
+    this.changeLastName = this.changeLastName.bind(this);
+    this.changeStatus = this.changeStatus.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
-
-  deleteManager(e) { 
-      let managers = this.state.managers
-      delete managers[parseInt(e.target.dataset.index, 10)]
-      this.setState({
-          managers : managers
+  changeLogin(e) { this.setState({ username: e.target.value })}
+  changeName(e) { this.setState({ firstname: e.target.value })}
+  changeLastName(e) { this.setState({ lastname: e.target.value })}
+  changeStatus(e) { this.setState({ status: e.target.value })}
+  deleteManager(index) { 
+    const managers = this.state.managers;
+    const deleteManager = () => {
+      delete managers[index];
+      this.setState({ managers, modal: {...this.state.modal, open: !this.state.modal.open} })
+    }
+    this.setState({ modal: {...this.state.modal, 
+      open: !this.state.modal.open,
+      title: 'Удаление менеджера',
+      body: `Вы действительно хотите удалить менеджера ${managers[index].username}?`,
+      ok: deleteManager,
+    }})
+  }
+  editManager(index) { 
+    const manager = this.state.managers[index];
+    const editManager = () => {
+      const { firstname, lastname, username, status } = this.state;
+      const managers = this.state.managers.map((m, k) => {
+        if(k === index) {
+          m.firstname = firstname;
+          m.lastname = lastname;
+          m.username = username;
+          m.status = status;
+        }
+        return m;
       })
+      this.setState({
+        modal: {...this.state.modal, open: !this.state.modal.open},
+        managers,
+        firstname: '',
+        lastname: '', 
+        username: '',
+        status: 0
+      })
+    }
+    const form = () => {
+      const { firstname, lastname, username, status } = this.state;
+      return <Form>
+        <FormGroup>
+          <label className="form-control-label" htmlFor="input-username" >Логин</label>
+          <Input
+            name="input-username"
+            className="form-control-alternative"
+            value={username}
+            onChange={this.changeLogin}
+            placeholder="Введите логин"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <label className="form-control-label" htmlFor="input-firstname" >Имя</label>
+          <Input
+            name="input-firstname"
+            className="form-control-alternative"
+            value={firstname}
+            onChange={this.changeName}
+            placeholder="Введите имя"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <label className="form-control-label" htmlFor="input-lastname" >Фамилия</label>
+          <Input
+            name="input-lastname"
+            className="form-control-alternative"
+            value={lastname}
+            onChange={this.changeLastName}
+            placeholder="Введите фамилия"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <label className="form-control-label" htmlFor="input-status" >Статус</label>
+          <Input
+            name="input-status"
+            className="form-control-alternative"
+            defaultValue={status}
+            onChange={this.changeStatus}
+            type="select"
+          >
+            {statuses.map((v, k) => (<option key={k} value={k}>{v}</option>))}
+          </Input>
+        </FormGroup>
+      </Form>
+    }
+
+    this.setState({ modal: {...this.state.modal, 
+        open: !this.state.modal.open,
+        title: 'Изменение менеджера',
+        body: form,
+        ok: editManager,
+      },
+      firstname: manager.firstname, 
+      lastname: manager.lastname, 
+      username: manager.username,
+      status: manager.status,
+    })
   }
 
+  toggle() {
+    this.setState({ modal: {...this.state.modal, open: !this.state.modal.open} })
+  }
 
   render() {
+    const { modal } = this.state;
     return (
       <>
+        <Modal isOpen={modal.open} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>{modal.title}</ModalHeader>
+          <ModalBody>{typeof modal.body === 'function' ? modal.body() : modal.body }</ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={modal.ok}>{modal.okTitle}</Button>{' '}
+            <Button color="secondary" onClick={modal.cancel}>{modal.cancelTitle}</Button>
+          </ModalFooter>
+        </Modal>
         <Header />
         {/* Page content */}
         <Container className="mt--7" fluid>
@@ -45,31 +187,36 @@ class Managers extends React.Component {
               <Card className="shadow">
                 <CardHeader className="border-0">
                 <h2>Менеджеры</h2>
-                    <table className="align-items-center table-flush" responsive>
-                        <thead>
-                            <tr className="mt-row">
-                                <th><h5>Менеджер</h5></th>
-                                <th>Имя</th>
-                                <th>Фамилия</th>
-                                <th>Дата</th>
-                                <th>Статус</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.managers.map(
-                                    (manager, index) => {
-                                        return (<Manager username={ manager.username } name={ manager.name }
-                                        surname={ manager.surname } date={ manager.date }
-                                        deleteManager={ this.deleteManager } index={ index }
-                                        status={ manager.status } key={ index }/>) 
-                                    }
-                                )
-                                
-                            }
-                        </tbody>
-                    </table>
                 </CardHeader>
+                <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                      <tr>
+                          <th>Логин</th>
+                          <th>Имя</th>
+                          <th>Фамилия</th>
+                          <th>Дата</th>
+                          <th>Статус</th>
+                          <th scope="col" />
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {
+                          this.state.managers.map(
+                              (manager, index) => {
+                                return <Manager username={ manager.username } name={ manager.firstname }
+                                  surname={ manager.lastname } date={ manager.date }
+                                  deleteManager={ this.deleteManager }
+                                  editManager={ this.editManager }
+                                  index={ index }
+                                  status={ manager.status }
+                                  key={ index }
+                                />
+                              }
+                          )
+                          
+                      }
+                  </tbody>
+                </Table>
               </Card>
             </div>
           </Row>
@@ -77,6 +224,49 @@ class Managers extends React.Component {
       </>
     )
   }
+}
+
+function Manager(props) {
+  return (
+      <tr>
+          <td>{ props.username }</td>
+          <td>{ props.name }</td>
+          <td>{ props.surname }</td>
+          <td>{ moment(props.date, 'YYYY-MM-DD hh:mm:ss').add(6, 'hours').format('DD.MM.YYYY HH:mm:ss') }</td>
+          <td>{ statuses[props.status] }</td>          
+          <td className="text-right">
+            <UncontrolledDropdown>
+              <DropdownToggle
+                className="btn-icon-only text-light"
+                href="#pablo"
+                role="button"
+                size="sm"
+                color=""
+                onClick={e => e.preventDefault()}
+              >
+                <i className="fas fa-ellipsis-v" />
+              </DropdownToggle>
+              <DropdownMenu className="dropdown-menu-arrow" right>
+                <DropdownItem
+                  onClick={() => props.deleteManager(props.index)}
+                >
+                  Удалить
+                </DropdownItem>
+                <DropdownItem
+                  onClick={() => props.editManager(props.index)}
+                >
+                  Изменить
+                </DropdownItem>
+                <DropdownItem
+                  onClick={e => e.preventDefault()}
+                >
+                  Something else here
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </td>
+      </tr>
+  )
 }
 
 export default Managers;
